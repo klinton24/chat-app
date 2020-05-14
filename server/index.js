8170bb474a1ee6f8148f501b44edf021c859f306
K Smith <klinton.jp@gmail.com>
2020-05-13 18:13:52 -0600

    server user commands

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -13,7 +15,25 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection!!");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name}, has joined room ${user.room}`,
+    });
+
+    socket.join(user.room);
+
+    callback();
+  });
 
   socket.on("disconnet", () => {
     console.log("User has left");
