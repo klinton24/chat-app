bef019dda09ad525435b7724d751cefe48f2de50
K Smith <klinton.jp@gmail.com>
2020-05-15 19:40:22 -0600

    }

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 5000;
 const router = require("./router");
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = socketio(server);
 
 io.on("connection", (socket) => {
